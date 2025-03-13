82b1ca5dfc9ec180b64805dd7cfe01a65469a575
dereje1212 <119671914+dereje1212@users.noreply.github.com>
2025-03-13 21:27:46 +0300

    Update productsApi.js

--- frontend/src/slices/productsApi.js
+++ frontend/src/slices/productsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://service-6kef.onrender.com" }),
   endpoints: (builder) => ({
     // Query to get all products
     getAllProducts: builder.query({
